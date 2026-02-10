76982d446c8f2454c48f3239a35124c495bf99e7
Archi-web3 <Archi-web3@users.noreply.github.com>
2026-02-10 11:51:37 +0100

    Fix API URL in prod environment

--- elat-app/src/environments/environment.prod.ts
+++ elat-app/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     // Sur Vercel, on peut surcharger via un "file replacement" ou utiliser une variable globale si configuré
     // Pour l'instant, on pointe vers l'URL du backend déployé que l'utilisateur nous donnera
     // OU mieux : on configure angular.json pour le remplacement de fichier
-    apiUrl: 'https://elat-api.onrender.com'
+    apiUrl: 'https://elat.onrender.com'
 };
