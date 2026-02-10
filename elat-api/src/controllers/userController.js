c0cca03327e465f89cafd829bfe4ed2a6e2c3b17
Archi-web3 <Archi-web3@users.noreply.github.com>
2026-02-10 14:11:01 +0100

    Fix user sorting field

--- elat-api/src/controllers/userController.js
+++ elat-api/src/controllers/userController.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 // Get All Users (Admin)
 exports.getUsers = async (req, res) => {
     try {
-        const users = await User.find().select('-password').sort({ date: -1 });
+        const users = await User.find().select('-password').sort({ createdAt: -1 });
         res.json(users);
     } catch (err) {
         console.error(err.message);
