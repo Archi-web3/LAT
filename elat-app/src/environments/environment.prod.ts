ad5a1ca84f104e2abff8f77abcc6d079a10b7b83
Archi-web3 <Archi-web3@users.noreply.github.com>
2026-02-12 14:34:48 +0100

    Fix: Optimize Layout for Mobile and Bump Version for PWA Update

--- elat-app/src/environments/environment.prod.ts
+++ elat-app/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
     // Sur Vercel, on peut surcharger via un "file replacement" ou utiliser une variable globale si configuré
     // Pour l'instant, on pointe vers l'URL du backend déployé que l'utilisateur nous donnera
     // OU mieux : on configure angular.json pour le remplacement de fichier
-    apiUrl: 'https://elat.onrender.com'
+    apiUrl: 'https://elat.onrender.com',
+    appVersion: '1.1.0'
 };
